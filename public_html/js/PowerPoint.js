ed68241610d2e0e53fab0994c8ccf6b2255094be
William Fyson <williamfyson89@hotmail.com>
2013-05-13 18:36:00 +0100

    Minor addition of PowerPoint returning a PowerPointImage when given an image name.

--- public_html/js/PowerPoint.js
+++ public_html/js/PowerPoint.js
@@ -6,15 +6,24 @@ function PowerPoint(){
 
     self.addImage = function(pptImage){
         self.pptImageArray.push(pptImage);
-    }
+    };
     
     self.addSlideImageRel = function(slideImageRel){
         self.slideImageRelArray.push(slideImageRel);
-    }
+    };
     
     self.getPptImageArray = function(){
         return self.pptImageArray;
-    }
+    };
+    
+    self.getPptImage = function(imageName){
+        for (var i = 0; i < self.pptImageArray.length; i++){
+            if (self.pptImageArray[i].name === imageName){
+                return self.pptImageArray[i];
+            }
+        }
+        return false;
+    };
 
 }
 
--- public_html/js/PowerPointViewer.js
+++ public_html/js/PowerPointViewer.js
@@ -1,17 +1,71 @@
 function PowerPointViewer(powerpoint){
 
     var self = this;
-    self.ppt = powerpoint;
+    self.powerpoint = powerpoint;
+    
+    //a function which takes the rels array and gets appropriate images
     
     self.displayImages = function(){
-        var rels = self.ppt.slideImageRelArray;
-        console.log(rels);
-        var images = self.ppt.pptImageArray;
-        for (var i = 0 ; i < images.length; i++){
-            var img = document.createElement('img');
-            $(img).attr("src", images[i].url);
-            $('#imageList').append(img);
+        var rels = self.powerpoint.slideImageRelArray;
+        //populate page with collapsible components to represent each slide
+        for (var i = 0; i < rels.length; i++){
+            var rel = rels[i];
+            var slide = rel.slide;
+            var imageName = rel.image;
+            if ($('#' + slide + "-accordion").length > 0){
+                //a collapsible component exists for this slide
+                //add the image
+                var img = document.createElement('img');
+                var pptImage = self.powerpoint.getPptImage(imageName);
+                $(img).attr("src", pptImage.url);
+                $('#' + slide + "-accordion-inner").append(img);               
+            }else{
+                //a collapsible component does not yet exist
+                var accordion = document.createElement("div");
+                $(accordion).attr("class", "accordion");
+                $(accordion).attr("id", slide + "-accordion");
+                
+                var accordionGroup = document.createElement("div");
+                $(accordionGroup).attr("class", "accordion-group");
+                
+                var accordionHeading = document.createElement("div");
+                $(accordionHeading).attr("class", "accordion-heading");
+                
+                var accordionToggle = document.createElement("a");
+                $(accordionToggle).attr("class", "accordion-toggle");
+                $(accordionToggle).attr("data-toggle", "collapse");
+                $(accordionToggle).attr("data-parent", "#" + slide + "-accordion");
+                $(accordionToggle).attr("href", "#" + slide + "-collapse");
+                                
+                var collapse = document.createElement("div");
+                $(collapse).attr("id", slide + "-collapse");
+                $(collapse).attr("class", "accordion-body collapse in");
+                
+                var accordionInner = document.createElement("div");
+                $(accordionInner).attr("class", "accordion-inner");
+                $(accordionInner).attr("id", slide + "-accordion-inner");
+                
+                //get image
+                var img = document.createElement('img');
+                var pptImage = self.powerpoint.getPptImage(imageName);
+                $(img).attr("src", pptImage.url);
+                
+
+                //add them all together                
+                $(accordionToggle).append(slide);
+                $(accordionInner).append($(img));
+                
+                $(accordionHeading).append($(accordionToggle));
+                
+                $(collapse).append($(accordionInner));
+                
+                $(accordionGroup).append($(accordionHeading));
+                $(accordionGroup).append($(collapse));
+                
+                $(accordion).append(accordionGroup);     
+                
+                $("#imageList").append($(accordion));
+            }
         }
     };
-    
 }
