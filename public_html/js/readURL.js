8851c732686a106330435ec4b528c7df769ff15d
rwf1v07 <rwf1v07@UOS-205394.soton.ac.uk>
2013-11-01 15:47:38 +0000

    Plugin version automatically detects file format and loads an appropriate reader.

--- public_html/js/readURL.js
+++ public_html/js/readURL.js
@@ -5,24 +5,38 @@ function handleFileSelect(name, blob) {
      * TODO - look at the file structure and determine if pptx or docx
      */
     
-    
-    //if ((name.substr(name.lastIndexOf("."))) === ".pptx") {
-        console.log(name);
-    
-        var pptReader = new PowerPointReader(name, blob);
-        powerpoint = pptReader.readPowerPoint();
-        //create a redactor
-        var powerpointRedactor = new PowerPointRedactor();
-        $('#redactBtn').data("redactor", powerpointRedactor);
-    //}else{
-    //    if ((name.substr(name.lastIndexOf("."))) === ".docx") {
-    //        var wordReader = new WordReader(name, blob);
-    //        word = wordReader.readWord();
-    //        //create a redactor
-    //        var wordRedactor = new WordRedactor();
-    //        $('#redactBtn').data("redactor", wordRedactor);
-     //   }else{
-            //none of the above so complain... or do we want to check this earlier on (in the JQuery plugin perhaps??)
-    //    }
-    //}
+    //read the blob object
+    zip.createReader(new zip.BlobReader(blob), function(reader){
+        reader.getEntries(function(entries){
+            for (var i = 0; i < entries.length; i++){      
+                if (entries[i].filename === "ppt/presentation.xml"){
+                    //dealing with a PowerPoint file
+                    var pptReader = new PowerPointReader(name, blob);
+                    powerpoint = pptReader.readPowerPoint();
+                    
+                    //create a redactor
+                    var powerpointRedactor = new PowerPointRedactor();
+                    $('#redactBtn').data("redactor", powerpointRedactor);                   
+                    
+                    //created a reader so now kill for loop
+                    break;
+                }
+                if (entries[i].filename === "word/document.xml"){
+                    //dealing with a Word file
+                    var wordReader = new WordReader(name, blob);
+                    word = wordReader.readWord();
+            
+                    //create a redactor
+                    var wordRedactor = new WordRedactor();
+                    $('#redactBtn').data("redactor", wordRedactor);
+                    
+                    //created a reader so now kill for loop
+                    break;
+                }
+            }
+            //not an appropriate file, so complain...
+            
+            
+        });
+    });
 }
