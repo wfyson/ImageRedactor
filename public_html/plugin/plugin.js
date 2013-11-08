bbc7c327a9f472fc65b9bb58ae4c67f940d25878
rwf1v07 <rwf1v07@UOS-205394.soton.ac.uk>
2013-11-08 18:08:35 +0000

    Plugin development.

--- public_html/plugin/plugin.js
+++ public_html/plugin/plugin.js
@@ -1,19 +1,26 @@
-(function($){
-    
-    var shade = "#556b2f";
-    
-    $.fn.redact = function(action){
-        
-        return this.each(function(action){
-            
+(function($) {
+
+    $.fn.redact = function(action) {
+
+        return this.each(function(action) {
+
             //add a modal with an iframe            
-            $('body').append("<div id=\"redact-modal\"><iframe src=\"http://users.ecs.soton.ac.uk/rwf1v07/redactor/\" width=\"100%\" height=\"100%\"></iframe>");                                                          
             
+            
+//$('body').append("<div id=\"redact-modal\"><iframe width=\"100%\" height=\"100%\" src=\"http://users.ecs.soton.ac.uk/rwf1v07/redactor/plugin.php?doc=" + this.href + "></iframe></div>");
+    
+            var url = "http://users.ecs.soton.ac.uk/rwf1v07/redactor/plugin.php?doc=" + this.href;
+            console.log(url);
+            url = encodeURI(url);
+            console.log(url);
+            $('body').append('<div id=\"redact-modal\"><iframe width=\"100%\" height=\"100%\" src=\"' + url + '"></iframe></div>');
+
+
             $('#redact-modal').dialog({
                 autoOpen: false,
                 width: 1200,
                 height: 700,
-                show: {                    
+                show: {
                     effect: "blind",
                     duration: 1000
                 },
@@ -23,14 +30,15 @@
                 },
                 modal: true
             });
-            
-            $(this).click(function(e){
-               e.preventDefault();
-               $('#redact-modal').dialog("open");
-               
+
+            $(this).click(function(e) {
+                e.preventDefault();
+                $('#redact-modal').dialog("open");
+
             });
 
-            return this;        
+            return this;
+
         });
     };
 }(jQuery));
